0d95932e3fb6cca161f57bd18d0b5254e4c89645
Dmitry Purtov <dmtr.pv@gmail.com>
2019-10-26 00:58:52 +0300

    Remove storyboards

---
+++ HereYouAR/HereYouAR/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  HereYouAR
+//
+//  Created by Dmitry Purtov on 26.10.2019.
+//  Copyright © 2019 texno-gipsy. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let rootVC = RootVC()
+        window?.rootViewController = rootVC
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+
+}
+
---
+++ HereYouAR/HereYouAR/RootVC.swift
@@ -0,0 +1,18 @@
+//
+//  RootVC.swift
+//  HereYouAR
+//
+//  Created by Dmitry Purtov on 26.10.2019.
+//  Copyright © 2019 texno-gipsy. All rights reserved.
+//
+
+import UIKit
+
+final class RootVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+}
